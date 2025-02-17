c63837e533d45bc5a07146cc425d5232c1441cef
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-17 23:30:29 +0700

    Create clear.ts

---
+++ src/clear.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from "./data-source";
+import { Order } from "./entity/Order";
+import { OrderItem } from "./entity/OrderItem";
+import { Product } from "./entity/Product";
+import { Role } from "./entity/Role";
+import { Type } from "./entity/Type";
+import { User } from "./entity/User";
+
+AppDataSource.initialize().then(async () => {
+    await AppDataSource.manager.clear(User);
+    await AppDataSource.manager.clear(Role);
+    await AppDataSource.manager.clear(Type);
+    await AppDataSource.manager.clear(Product);
+    await AppDataSource.manager.clear(OrderItem);
+    await AppDataSource.manager.clear(Order);
+})
