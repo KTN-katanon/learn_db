76bb84c2f5443d127ce0454a9588742d76e43e01
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-17 22:00:38 +0700

    Create Product.ts

---
+++ src/entity/Product.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm"
+import { Type } from "./Type"
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    price: number
+
+    @CreateDateColumn()
+    created: Date
+
+    @UpdateDateColumn()
+    updated: Date
+
+    @ManyToOne(() => Type, type => type.product)
+    type: Type
+}
